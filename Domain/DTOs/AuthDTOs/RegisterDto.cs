178f83827a5913f9eee3d3f6800f1354ae428487
SiyovushK <kurbonovs397@gmail.com>
2025-05-10 13:40:56 +0500

    done

---
+++ Domain/DTOs/AuthDTOs/RegisterDto.cs
@@ -0,0 +1,11 @@
+namespace Domain.DTOs.AuthDTOs;
+
+public class RegisterDto
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime BirthDate { get; set; }
+}
---
+++ Domain/DTOs/StudentDTOs/CreateStudentDTO.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.DTOs.StudentDTOs;
+
+public class CreateStudentDTO
+{
+    [Required]
+    public string Username { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime BirthDate { get; set; }
+}
